e0418ddcc3957d433b7dc6e1eb3b399b6fde056a
Jason Huang <jasonkwh@gmail.com>
2024-12-13 23:17:14 +0100

    remove not necessary viewpoint define

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <svelte:head>
 	<title>Wedding Invitation</title>
 	<meta name="description" content="Jason & Sherry">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 </svelte:head>
